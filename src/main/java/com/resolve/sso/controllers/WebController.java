da752337e98c5d46e4755951f449743c10d6bce8
Tao Li <tao.li@fixstream.com>
2020-02-25 13:39:54 -0800

    adding @ComponentScan, logging config

--- src/main/java/com/resolve/sso/controllers/WebController.java
+++ src/main/java/com/resolve/sso/controllers/WebController.java
@@ -1,12 +1,20 @@
 package com.resolve.sso.controllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 @RestController
 public class WebController {
+	private static Logger logger = LogManager.getLogger(WebController.class);
 	
 		@RequestMapping("/")
 		public String index() {
+			int n=5;
+			logger.debug("test {},---{}",n,(n+5));
+			logger.debug("test ");
 			return "Greetings from Spring Boot";
 		}
 	
